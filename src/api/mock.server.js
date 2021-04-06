594d9399996d794e0907c1e88074d30f93c58063
prerana1821 <prerananawar1@gmail.com>
2021-04-06 21:14:10 +0530

    Removed demo item form cart

--- src/api/mock.server.js
+++ src/api/mock.server.js
@@ -56,40 +56,8 @@ export default function setupMockServer() {
           color: faker.commerce.color(),
         });
       });
-      [...Array(1)].forEach((_) => {
-        server.create("cartItem", {
-          id: faker.datatype.uuid(),
-          name: faker.commerce.productName(),
-          image: faker.random.image(),
-          quantity: 1,
-          price: faker.commerce.price(),
-          material: faker.commerce.productMaterial(),
-          brand: faker.lorem.word(),
-          inStock: faker.datatype.boolean(),
-          wishList: faker.datatype.boolean(),
-          fastDelivery: faker.datatype.boolean(),
-          ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
-          offer: faker.random.arrayElement([
-            "Save 50",
-            "70% bonanza",
-            "Republic Day Sale",
-          ]),
-          idealFor: faker.random.arrayElement([
-            "Men",
-            "Women",
-            "Girl",
-            "Boy",
-            "Senior",
-          ]),
-          level: faker.random.arrayElement([
-            "beginner",
-            "amateur",
-            "intermediate",
-            "advanced",
-            "professional",
-          ]),
-          color: faker.commerce.color(),
-        });
+      [...Array(0)].forEach((_) => {
+        server.create("cartItem", {});
       });
       [...Array(0)].forEach((_) => {
         server.create("wishList", {});
